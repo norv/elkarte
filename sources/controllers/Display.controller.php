079a3dc57bd6ac7498de58808d2d3ab07e145097
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2013-05-16 22:38:49 -0400

    Add getTopicsEvents() to get events for one or more topics at a time Removed that query from Display.controller.php Signed-off-by:Joshua Dickerson <joshua.a.dickerson@gmail.com>

--- sources/controllers/Display.controller.php
+++ sources/controllers/Display.controller.php
@@ -385,47 +385,8 @@ class Display_Controller
 		// If we want to show event information in the topic, prepare the data.
 		if (allowedTo('calendar_view') && !empty($modSettings['cal_showInTopic']) && !empty($modSettings['cal_enabled']))
 		{
-			// First, try create a better time format, ignoring the "time" elements.
-			if (preg_match('~%[AaBbCcDdeGghjmuYy](?:[^%]*%[AaBbCcDdeGghjmuYy])*~', $user_info['time_format'], $matches) == 0 || empty($matches[0]))
-				$date_string = $user_info['time_format'];
-			else
-				$date_string = $matches[0];
-
-			// Any calendar information for this topic?
-			$request = $smcFunc['db_query']('', '
-				SELECT cal.id_event, cal.start_date, cal.end_date, cal.title, cal.id_member, mem.real_name
-				FROM {db_prefix}calendar AS cal
-					LEFT JOIN {db_prefix}members AS mem ON (mem.id_member = cal.id_member)
-				WHERE cal.id_topic = {int:current_topic}
-				ORDER BY start_date',
-				array(
-					'current_topic' => $topic,
-				)
-			);
-			$context['linked_calendar_events'] = array();
-			while ($row = $smcFunc['db_fetch_assoc']($request))
-			{
-				// Prepare the dates for being formatted.
-				$start_date = sscanf($row['start_date'], '%04d-%02d-%02d');
-				$start_date = mktime(12, 0, 0, $start_date[1], $start_date[2], $start_date[0]);
-				$end_date = sscanf($row['end_date'], '%04d-%02d-%02d');
-				$end_date = mktime(12, 0, 0, $end_date[1], $end_date[2], $end_date[0]);
-
-				$context['linked_calendar_events'][] = array(
-					'id' => $row['id_event'],
-					'title' => $row['title'],
-					'can_edit' => allowedTo('calendar_edit_any') || ($row['id_member'] == $user_info['id'] && allowedTo('calendar_edit_own')),
-					'modify_href' => $scripturl . '?action=post;msg=' . $topicinfo['id_first_msg'] . ';topic=' . $topic . '.0;calendar;eventid=' . $row['id_event'] . ';' . $context['session_var'] . '=' . $context['session_id'],
-					'can_export' => allowedTo('calendar_edit_any') || ($row['id_member'] == $user_info['id'] && allowedTo('calendar_edit_own')),
-					'export_href' => $scripturl . '?action=calendar;sa=ical;eventid=' . $row['id_event'] . ';' . $context['session_var'] . '=' . $context['session_id'],
-				'start_date' => standardTime($start_date, $date_string, 'none'),
-					'start_timestamp' => $start_date,
-				'end_date' => standardTime($end_date, $date_string, 'none'),
-					'end_timestamp' => $end_date,
-					'is_last' => false
-				);
-			}
-			$smcFunc['db_free_result']($request);
+			require_once(SUBSDIR . 'Calendar.subs.php');
+			$context['linked_calendar_events'] = getTopicsEvents($topic);
 
 			if (!empty($context['linked_calendar_events']))
 				$context['linked_calendar_events'][count($context['linked_calendar_events']) - 1]['is_last'] = true;
