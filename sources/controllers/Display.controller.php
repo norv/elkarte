560b2c42f284d957e8e0c01177910c54e6cf3f3c
Joshua Dickerson <joshua.a.dickerson@gmail.com>
2013-05-16 23:31:15 -0400

    whitespace Signed-off-by:Joshua Dickerson <joshua.a.dickerson@gmail.com>

--- sources/controllers/Display.controller.php
+++ sources/controllers/Display.controller.php
@@ -1181,7 +1181,6 @@ function loadAttachmentContext($id_msg)
 					$attachment = $attachment + updateAttachmentThumbnail($filename, $attachment['id_thumb']);
 				}
 	
-
 				// Only adjust dimensions on successful thumbnail creation.
 				if (!empty($attachment['thumb_width']) && !empty($attachment['thumb_height']))
 				{
